4a3b74af95d5e9f468a050b978218e971a0a9a78
Andres Jimenez <afjimenezb@bncr.fi.cr>
2024-05-11 12:38:00 -0500

    Creating the first structure for the project

---
+++ products/src/test/java/com/flybank/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flybank.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
