de1d04a47e1b1c7154207ed39da0f43d862d1d3f
Andres Jimenez <afjimenezb@bncr.fi.cr>
2024-05-13 13:01:23 -0500

    Including service unit test

---
+++ clients/src/test/java/com/flybank/clients/services/ClientServiceTest.java
@@ -0,0 +1,37 @@
+package com.flybank.clients.services;
+
+import com.flybank.clients.entities.Client;
+import com.flybank.clients.repository.ClientRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+
+@SpringBootTest
+class ClientServiceTest {
+
+    @Autowired
+    ClientService clientService;
+
+    @MockBean
+    ClientRepository repository;
+
+    @Test
+    void ClientService_CreateClient_ReturnsClientDto() {
+        var client = new Client();
+        client.setId(12345L);
+        client.setNames("test1");
+
+        given(repository.save(Mockito.any(Client.class))).willReturn(client);
+
+        var savedClient = clientService.createClient(client);
+
+        assertNotNull(savedClient);
+        assertEquals(12345L, savedClient.getId());
+        assertEquals("test1", savedClient.getNames());
+    }
+}
